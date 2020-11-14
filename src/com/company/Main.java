084aef3547821606e03bebc3a0b2619ecdd1c471
wp <dmitroua99@gmail.com>
2020-11-14 10:03:10 +0100

    fix commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,40 +7,40 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int i = scanner.nextInt();
 
-//        do {
-//            System.out.println("Choose:");
-//            System.out.println("1. Obliczenie pola prostokąta");
-//            System.out.println("2. Obliczenie pola kwadrat");
-//            System.out.println("3. Obliczenie pola trójkąta");
-//            System.out.println("0. Zakończ1 program");
-//            switch (i) {
-//                case 1:
-//                    System.out.println("Podaj bok prostokąta:");
-//                    int a = scanner.nextInt();
-//                    System.out.println("Podaj drugi bok prostokąta:");
-//                    int b = scanner.nextInt();
-//                    System.out.println("Pole prostokąta wynosi:" + a*b);
-//                    break;
-//                case 2:
-//                    System.out.println("Podaj bok kwadrata:");
-//                    int c = scanner.nextInt();
-//                    System.out.println("Podaj drugi bok kwadrata:");
-//                    int d = scanner.nextInt();
-//                    System.out.println("Pole kwadrat wynosi:" + c*d);
-//                    break;
-//                case 3:
-//                    System.out.println("Podaj bok trójkąta:");
-//                    int e = scanner.nextInt();
-//                    System.out.println("Podaj wysokość trójkąta:");
-//                    int j = scanner.nextInt();
-//                    System.out.println("Pole trójkąta wynosi:" + e*j);
-//                    break;
-//                case 0:
-//                    break;
-//            }
-//        } while (i != 0); {
-//
-//        }
+        do {
+            System.out.println("Choose:");
+            System.out.println("1. Obliczenie pola prostokąta");
+            System.out.println("2. Obliczenie pola kwadrat");
+            System.out.println("3. Obliczenie pola trójkąta");
+            System.out.println("0. Zakończ1 program");
+            switch (i) {
+                case 1:
+                    System.out.println("Podaj bok prostokąta:");
+                    int a = scanner.nextInt();
+                    System.out.println("Podaj drugi bok prostokąta:");
+                    int b = scanner.nextInt();
+                    System.out.println("Pole prostokąta wynosi:" + a*b);
+                    break;
+                case 2:
+                    System.out.println("Podaj bok kwadrata:");
+                    int c = scanner.nextInt();
+                    System.out.println("Podaj drugi bok kwadrata:");
+                    int d = scanner.nextInt();
+                    System.out.println("Pole kwadrat wynosi:" + c*d);
+                    break;
+                case 3:
+                    System.out.println("Podaj bok trójkąta:");
+                    int e = scanner.nextInt();
+                    System.out.println("Podaj wysokość trójkąta:");
+                    int j = scanner.nextInt();
+                    System.out.println("Pole trójkąta wynosi:" + e*j);
+                    break;
+                case 0:
+                    break;
+            }
+        } while (i != 0); {
+
+        }
 
 
     }
